a6c1ef22bb6ef72b23d5180b1abc00ecc57b416e
Li Feilong <feiyang8068@qq.com>
2015-05-23 15:22:01 +0800

    format

--- worker/celeryconfig.py
+++ worker/celeryconfig.py
@@ -1,6 +1,5 @@
-'''
-Celery config
-'''
+# -*- coding: utf-8 -*-
+# celery config
 
 BROKER_URL = 'redis://127.0.1:6379/0'
 CELERY_BACKEND = 'redis://127.0.1:6379/0'
--- worker/tasks.py
+++ worker/tasks.py
@@ -1,5 +1,7 @@
-from __future__ import absolute_import
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
 from celery import Celery, Task
 
 celery = Celery('tasks')
